c0c18961dce7c37348866a3c600386a5a5ba631a
JulianaAparicio <juliana_aparicio@outlook.com>
2023-04-02 18:05:23 -0300

    Added new getSerieByGenre endpoint

--- api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
+++ api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
@@ -2,6 +2,7 @@ package com.dh.catalog.controller;
 
 import com.dh.catalog.client.MovieServiceClient;
 
+import com.dh.catalog.client.SerieServiceClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,17 +16,25 @@ import java.util.List;
 public class CatalogController {
 
 	//TODO:
-	// Acomodar para que devuelve al buscar por género series y peliculas.
+	// Acomodar para que devuelve al buscar por género series y peliculas (2 ENDPOINTS HAY QUE HACER).
 
 	private final MovieServiceClient movieServiceClient;
+	private final SerieServiceClient serieServiceClient;
 
-	public CatalogController(MovieServiceClient movieServiceClient) {
+	public CatalogController(MovieServiceClient movieServiceClient, SerieServiceClient serieServiceClient) {
 		this.movieServiceClient = movieServiceClient;
+		this.serieServiceClient = serieServiceClient;
 	}
 
-	@GetMapping("/{genre}")
-	ResponseEntity<List<MovieServiceClient.MovieDto>> getGenre(@PathVariable String genre) {
+
+	@GetMapping("/movies/{genre}")
+	ResponseEntity<List<MovieServiceClient.MovieDto>> getMovieGenre(@PathVariable String genre) {
 		return ResponseEntity.ok(movieServiceClient.getMovieByGenre(genre));
+	}
+
+	@GetMapping("/series/{genre}")
+	ResponseEntity<List<SerieServiceClient.SerieDto>> getSerieGenre(@PathVariable String genre) {
+		return ResponseEntity.ok(serieServiceClient.getSerieByGenre(genre));
 	}
 
 }
